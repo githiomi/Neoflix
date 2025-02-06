28d520d12417f657cc888d3bf1b85208628be21f
githiomi <daniel.githiomi@gmail.com>
2025-02-06 10:58:47 +0400

    Feat: Added favourite click functionality

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,46 +1,68 @@
 import { Movie } from "@/data/Movie"
 import '../index.css';
 import Image from "./Image";
-import { FaStar } from "react-icons/fa";
+import { FaHeart, FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 type MovieCardProps = {
    movie: Movie
 };
 
-const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language, adult } }: MovieCardProps) => {
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language, adult, genre_ids } }: MovieCardProps) => {
+
+   const [isFavourite, setIsFavourite] = useState(false);
+
+   const handleFavouriteClick = () => {
+      setIsFavourite((currentValue) => !currentValue);
+   };
+
    return (
       <div className="movie-card">
 
-         <Image imageUrl={poster_path} alt={`${title} Poster`} />
+         <div className="overlay">
 
-         <div className="movie-details mt-4 mx-2">
-            <div className="movie-title flex flex-row gap-4">
-               <h3 className="text-red-600 text-xl uppercase">{title}</h3>
-               {adult && <span className="movie-extra">PG-13</span>}
+            <div className="favourite" onClick={handleFavouriteClick}>
+               <FaHeart className={isFavourite ? 'text-red-600' : 'text-white'} size={15} />
             </div>
 
-            <div className="movie-content flex flex-col items-start gap-4">
+            <Image imageUrl={poster_path} alt={`${title} Poster`} />
 
-               <div className="movie-rating flex flex-row gap-4 items-center">
-                  <FaStar className="text-amber-400" size={20} />
-                  <p className="text-white font-bold">
-                     {vote_average
-                        ? vote_average.toFixed(2)
-                        : "No Rating Available"}
-                  </p>
+            <div className="movie-details mt-4 mx-2">
+               <div className="movie-title flex flex-row gap-4">
+                  <h3 className="text-red-600 text-xl uppercase">{title}</h3>
+                  {adult && <span className="movie-extra">PG-13</span>}
                </div>
 
-               <div className="movie-release flex flex-row items-center gap-4">
-                  <p className="movie-extra">{original_language}</p>
-                  <span className="text-amber-400">•</span>
-                  <p className="year">
-                     {release_date
-                        ? release_date.split('-')[0]
-                        : "No Release Date"}
-                  </p>
-               </div>
+               <div className="movie-content flex flex-col items-start gap-4">
 
+                  <div className="movie-rating flex flex-row gap-4 items-center">
+                     <FaStar className="text-amber-400" size={20} />
+                     <p className="text-white font-bold">
+                        {vote_average
+                           ? vote_average.toFixed(2)
+                           : "No Rating Available"}
+                     </p>
+                  </div>
+
+                  <div className="movie-release flex flex-row items-center gap-4">
+                     <p className="movie-extra">{original_language}</p>
+                     <span className="text-amber-400">•</span>
+                     <p className="year">
+                        {release_date
+                           ? release_date.split('-')[0]
+                           : "No Release Date"}
+                     </p>
+                  </div>
+
+                  <div className="genres text-amber-400">
+                     {genre_ids
+                        ? genre_ids.map(genre_id => <span>{genre_id} - </span>)
+                        : "No Genres Available"}
+                  </div>
+
+               </div>
             </div>
+
          </div>
 
       </div>
